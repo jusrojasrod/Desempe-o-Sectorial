b4a74e83262b41c061467b73cfc71eec1ba4f2b8
jusrojasrod <rebelion.96@gmail.com>
2023-11-01 15:36:50 -0500

    added threading to download data function

--- ETFs/data.py
+++ ETFs/data.py
@@ -1,12 +1,24 @@
-import config
 import requests
 import pandas as pd
 
 import os
 import time
+import concurrent.futures
+import threading
+from functools import partial
+
+import config
+
+thread_local = threading.local()
 
 
-def downloadData(ticker, start, end, period='d'):
+def get_session():
+    if not hasattr(thread_local, "session"):
+        thread_local.session = requests.Session()
+    return thread_local.session
+
+
+def downloadTicker(ticker, start, end, period='d'):
     """
     """
     cwd = os.getcwd()
@@ -20,27 +32,36 @@ def downloadData(ticker, start, end, period='d'):
                'from': start,
                'to': end,
                'fmt': 'json'}
-
-    session = requests.Session()
+    session = get_session()
     r = session.get(_BASE_URL_, params=payload)
     data = r.json()
     df = pd.DataFrame(data)
     df.to_pickle(f'{path_results}{ticker.upper()}')
+    # df.to_excel(f'{path_results}{ticker.upper()}.xlsx')
+    # print(f"{ticker}: {r}")
+
+
+def downloadAllTickers(tickers, start, end):
+    """
+    """
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        executor.map(partial(downloadTicker, start=start, end=end), tickers)
 
 
 if __name__ == "__main__":
 
     start = '2023-05-01'
     end = '2023-05-02'
-    ticker = 'TSLA'
+    tickers = ["CHIS", "CLIX"]
 
     start_time = time.time()
-    downloadData(ticker, start, end)
+    downloadAllTickers(tickers, start=start, end=end)
     duration = time.time() - start_time
     print(f"Elapsed time: {duration:.2f} seconds")
-    cwd = os.getcwd()
-    folderPath_rsrc = '/ETFs/Resources/'
-    path_results = cwd + folderPath_rsrc
-    data = pd.read_pickle(f"{path_results}{ticker}")
 
-    print(data)
+    # cwd = os.getcwd()
+    # folderPath_rsrc = '/ETFs/Resources/'
+    # path_results = cwd + folderPath_rsrc
+    # # data = pd.read_pickle(f"{path_results}{tickers[0]}")
+
+    # print()

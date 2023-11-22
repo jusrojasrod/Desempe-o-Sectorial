32cff520d551ce7768492ba1fd0633d628bad39b
jusrojasrod <rebelion.96@gmail.com>
2023-11-22 12:30:29 -0500

    updated completed

--- ETFs/backtesting.py
+++ ETFs/backtesting.py
@@ -1,27 +1,54 @@
 # import libraries
-import plot
-
+import pandas as pd
+import time
 import os
+from datetime import timedelta
 
-# Parent directory path
-PARENT_DIR = os.getcwd()
-
-# new folder
-new_folder = "date"
-
-# Existing Directory
-directory = f"Pictures/{new_folder}"
-
-print(PARENT_DIR)
-
-# Path
-path = os.path.join(PARENT_DIR, directory)
-
-try:
-    os.mkdir(path)
-    print(path)
-except OSError as error:
-    print(error)
+from tools import dates_list
+import main
 
 
+def backtesting(years=0, months=0, days=0):
+    """
+    """
 
+    # setup
+    folderPath_rsrc = '/ETFs/Resources/'
+    cwd = os.getcwd()
+    path_rsrc = cwd + folderPath_rsrc
+
+    # read data
+    ETFs = pd.read_excel(path_rsrc + "ETFs list.xlsx")
+
+    dates = dates_list(years=years, months=months, days=days)
+    # Strategy for sectors itself
+    for date in dates:
+        end = date + timedelta(weeks=3)
+        main.run_strategy_sectors(end=end, start=date,
+                                  column_name='Close',
+                                  showFig=False,
+                                  fig_path=str(date).split(" ")[0])
+
+    for date in dates:
+        print("\n", f"|-> Date: [{date}] Begin execution", "\n")
+        end = date + timedelta(weeks=3)
+        # Strategy [component sectors]
+        for sector in ETFs.columns:
+            print("\t", f"--> {sector}")
+            print(main.run_strategy(sector=sector, end=end, start=date,
+                                    tickers=ETFs[sector].dropna().to_list(),
+                                    column_name='Close',
+                                    fig_path=str(date).split(" ")[0]))
+            print("\t", "="*100)
+
+
+if __name__ == "__main__":
+    # Start timer
+    start_time = time.time()
+
+    # backtesting
+    backtesting(years=2)
+
+    # Calculate elapsed time
+    elapsed_time = time.time() - start_time
+    print("Elapsed time: ", elapsed_time)

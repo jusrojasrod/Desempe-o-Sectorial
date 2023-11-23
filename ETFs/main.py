b603af57fb40f80b4cef220b2939b101bad65865
jusrojasrod <rebelion.96@gmail.com>
2023-11-23 08:07:40 -0500

    added
    Conditionals for excel or plot output

--- ETFs/main.py
+++ ETFs/main.py
@@ -29,7 +29,8 @@ import sectors
 
 def run_strategy(sector, end, start,
                  tickers, fig_path, column_name='Close',
-                 showFig=False):
+                 showFig=False,
+                 figure=False):
     """
     """
 
@@ -53,21 +54,28 @@ def run_strategy(sector, end, start,
     # Select best and worst ETFs
     selection_ = tools.selection(last_row, n=10)
 
-    # ## Plot
-    # Matrix formation
-    new_names, new_array = plot.matrix_to_plot(selection_)
+    if figure:
+        # ## Plot
+        # Matrix formation
+        new_names, new_array = plot.matrix_to_plot(selection_)
 
-    # Plot
-    plot.heatmap(values=new_array, labels=new_names,
-                 max_=val_max, min_=val_min, sector=sector, show=showFig,
-                 path=week_date)
+        # Plot
+        plot.heatmap(values=new_array, labels=new_names,
+                     max_=val_max, min_=val_min, sector=sector, show=showFig,
+                     path=week_date)
+
+    else:
+        path = tools.create_dir(existing_directory="ETFs/results",
+                                new_folder=week_date)
+        selection_.to_excel(f"{path}/{sector}.xlsx")
 
     return selection_
 
 
 def run_strategy_sectors(end, start,
                          fig_path, column_name='Close',
-                         showFig=False):
+                         showFig=False,
+                         figure=False):
     # =================== Sectors ==============================
 
     ETF_sectors = sectors.ETF_sectors
@@ -87,11 +95,16 @@ def run_strategy_sectors(end, start,
     week_date = str(last_sec_ret.name).split(" ")[0]
 
     # Plot sectors
-    plot.plot_bar_sectors(x=last_sec_ret.index,
-                          y=(last_sec_ret.values*100).round(2),
-                          show=showFig,
-                          path=week_date)
+    if figure:
+        plot.plot_bar_sectors(x=last_sec_ret.index,
+                              y=(last_sec_ret.values*100).round(2),
+                              show=showFig,
+                              path=week_date)
 
+    else:
+        path = tools.create_dir(existing_directory="ETFs/results",
+                                new_folder=week_date)
+        last_sec_ret.to_excel(f"{path}/sectorial.xlsx")
     # ==================================================================
 
 
@@ -102,7 +115,7 @@ if __name__ == "__main__":
 
     # setup
     folderPath_rsrc = '/ETFs/Resources/'
-    folderPath_results = '/ETFs/Results/'
+    folderPath_results = '/ETFs/results/'
     pictures = '/Pictures/'
     cwd = os.getcwd()
     path_rsrc = cwd + folderPath_rsrc
@@ -122,6 +135,7 @@ if __name__ == "__main__":
         print(f"---> {sector}")
         print(run_strategy(sector=sector, end=end_, start=start_,
                            tickers=ETFs[sector].dropna().to_list(),
+                           figure=False,
                            column_name='Close',
                            fig_path=str(start_).split(" ")[0]))
         print("="*100)
@@ -130,6 +144,7 @@ if __name__ == "__main__":
     run_strategy_sectors(end=end_, start=start_,
                          column_name='Close',
                          showFig=False,
+                         figure=False,
                          fig_path=str(start_).split(" ")[0])
 
     # Calculate elapsed time
